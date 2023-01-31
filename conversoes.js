1b1decd92edf5ba7998e65ae5c60f3e2e23a7a76
Utahh <kaw.alveslima@gmail.com>
2023-01-31 15:21:44 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+//booleans 
+
+//conversao implícita 
+
+const numero = 34;
+const numeroString = Number("34");
+
+//Number()
+//String()
+
+
+console.log(numero + numeroString);
+//conversao explicita 
