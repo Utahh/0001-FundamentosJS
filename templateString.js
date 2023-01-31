8f71cd34fae2e6c9983caf14e83ea8b2e196da8e
Utahh <kaw.alveslima@gmail.com>
2023-01-31 16:51:52 -0300

    templateString

---
+++ templateString.js
@@ -0,0 +1,14 @@
+const nome = "Cauan";
+const idade = 2022-1998;
+const cidadeDeNascimento = "Parapuã-SP"
+
+
+
+
+
+
+const apresentacao = `
+    meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}.
+`;
+
+console.log(apresentacao);
