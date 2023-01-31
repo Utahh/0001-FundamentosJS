1d6a142d24c5f30dfc05f766428bab675202e432
Utahh <kaw.alveslima@gmail.com>
2023-01-31 13:16:19 -0300

    Exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// type number 
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const opercaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(opercaoMatematica);
+
+// ponto flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante / pontoFlutuanteSemZero;
+
+console.log(novaOperacao);
+
+//NaN -> Not a Number 
+
+const cauan = "Cauan" * numeroPontoFlutuante;
+console.log(cauan); 
+
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá mundo!";
+const texto2 = "1";
+const senha = "senha123";
+const citacao = "Meu nome é ";
+const meuNome = "Cauan";
+
+
+
+// concatenação (+)
+console.log(citacao + meuNome);
+
+
+//template string OU template literal 
+
