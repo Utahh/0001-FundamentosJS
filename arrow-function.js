0cc77b33298872c5822661df3cdb3595bf967df4
Utahh <kaw.alveslima@gmail.com>
2023-02-02 12:56:03 -0300

    Operadores e funções

---
+++ arrow-function.js
@@ -0,0 +1,22 @@
+// antes 
+
+function apresentar(nome){
+    return`meu nome é ${nome}`;
+}
+
+// Arrow function
+const apresentarArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+// Arrow function com mais de uma linha de instrução 
+
+const somaNumerosPequenos = (num1, num2) => {
+    if (num1 || num2 > 10){
+        return "Somente número de 1 a 9"
+    }else{
+        return num1 + num2;
+    }
+}
+
+// Hoisting: arrow function se comportam como expressão 
+
--- parametros.js
+++ parametros.js
@@ -23,7 +23,7 @@ function soma(numero1, numero2) {
    return numero1 + numero2;
 }
 
-function multiplica(numero1, numero2) {
+function multiplica(numero1 = 1, numero2 = 1) {
     return numero1 * numero2;
 }
 
